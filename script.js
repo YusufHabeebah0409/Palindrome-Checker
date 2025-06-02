9bebb2465125aff2dca1776a1f7bb45c0d9bb746
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-06-02 16:41:00 +0100

    Enhance palindrome checker UI and functionality with audio feedback

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+const succesAudio = new Audio("success.mp3");
+const slowSad = new Audio("slowsad.wav");
 const checkPalindromeButton = () => {
-    if(palindromeInput.value === ""){
+    if (palindromeInput.value === "") {
         alert("Input a name ")
-    }else{
-    let reverseStr = palindromeInput.value.trim().toLowerCase()
-    let newStr = reverseStr.split("").reverse().join("")
-    console.log(newStr);
-    
-    palindromeInput.value = ""
+    } else {
+        let reverseStr = palindromeInput.value.trim().toLowerCase()
+        let newStr = reverseStr.split("").reverse().join("")
+        console.log(newStr);
+
+        if (newStr === reverseStr) {
+            succesAudio.play();
+            show.innerHTML = ""
+            show.innerHTML = `
+        <p>Yes! Its a palindrome 😍</p>
+        `
+        } else {
+            slowSad.play();
+            show.innerHTML = ""
+            show.innerHTML = `
+        <p>Nope! Not a palindrome 💔</p>
+        `
+        }
+        setTimeout(() => {
+            palindromeInput.value = ""
+            show.innerHTML = `<p>Your result will show here 😇🤩😇</p>`
+        }, 2000)
     }
-    
+
 }
 
 
