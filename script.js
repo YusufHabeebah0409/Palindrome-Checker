7413942233008f6f73bd9018f2573f1d3d9f70fd
YusufHabeebah0409 <ojuolapehabeebah@gmail.com>
2025-06-03 14:48:30 +0100

    Fix formatting inconsistencies in HTML, CSS, and JavaScript for improved readability

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const succesAudio = new Audio("success.mp3");
 const slowSad = new Audio("slowsad.wav");
 
-const toast = (text,background,color) => {
- Toastify({
-  text,
-  duration: 3000,
-  newWindow: true,
-  close: true,
-  gravity: "top", // `top` or `bottom`
-  position: "right", // `left`, `center` or `right`
-  stopOnFocus: true, // Prevents dismissing of toast on hover
-  style: {
-    background,
-    color,
-  },
-  onClick: function(){} // Callback after click
-}).showToast();
+const toast = (text, background, color) => {
+    Toastify({
+        text,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+            background,
+            color,
+        },
+        onClick: function () { } // Callback after click
+    }).showToast();
 }
 
 
@@ -27,7 +27,7 @@ const checkPalindromeButton = () => {
         let newStr = reverseStr.split("").reverse().join("")
         console.log(newStr);
 
-            sub.innerHTML = `      
+        sub.innerHTML = `      
     <div class="dot-spinner">
     <div class="dot-spinner__dot"></div>
     <div class="dot-spinner__dot"></div>
@@ -40,24 +40,24 @@ const checkPalindromeButton = () => {
     </div>
             `
         setTimeout(() => {
-             if (newStr === reverseStr) {
-            succesAudio.play();
-            show.innerHTML = ""
-            show.innerHTML = `
+            if (newStr === reverseStr) {
+                succesAudio.play();
+                show.innerHTML = ""
+                show.innerHTML = `
         <p>Yes! Its a palindrome 😍</p>
         `
-        } else {
-            slowSad.play();
-            show.innerHTML = ""
-            show.innerHTML = `
+            } else {
+                slowSad.play();
+                show.innerHTML = ""
+                show.innerHTML = `
         <p>Nope! Not a palindrome 💔</p>
         `
-        }
-        setTimeout(() => {
-            palindromeInput.value = ""
-            show.innerHTML = `<p>Your result will show here 😇🤩😇</p>`
-        }, 4000)
-        sub.innerHTML =`
+            }
+            setTimeout(() => {
+                palindromeInput.value = ""
+                show.innerHTML = `<p>Your result will show here 😇🤩😇</p>`
+            }, 4000)
+            sub.innerHTML = `
                      <span>Check</span>
         `
         }, 2500);
